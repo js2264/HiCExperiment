0d81fabc0a335d2bdb958b1220557b44f65b7e55
js2264 <jacquesserizay@gmail.com>
2023-04-22 18:36:39 +0200

    add onLoad message

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+.onLoad <- function(libname, pkgname) {
+    packageStartupMessage(
+"Consider using the `HiContacts` package to perform advanced genomic operations 
+on `HiCExperiment` objects.
+
+Read \"Orchestrating Hi-C analysis with Bioconductor\" online book to learn more:
+https://js2264.github.io/OHCA/"
+    )
+}
